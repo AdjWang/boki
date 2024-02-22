0964f05bb278faf9b4360fb0b5860ff0541b4e8f
adjwang <wwang230513@gmail.com>
2024-02-22 06:52:23 +0000

    add cpu overall usage report

--- src/utils/docker.cpp
+++ src/utils/docker.cpp
@@ -39,6 +39,64 @@ bool ReadCpuAcctStat(std::string_view container_id, int32_t* user, int32_t* syst
     }
     return true;
 }
+
+// /proc/stat
+bool ReadCpuProcStat(int32_t* user,
+                     int32_t* nice,
+                     int32_t* system,
+                     int32_t* idle,
+                     int32_t* iowait,
+                     int32_t* irq,
+                     int32_t* softirq,
+                     int32_t* steal,
+                     int32_t* guest,
+                     int32_t* guest_nice) {
+    std::string full_path("/proc/stat");
+    std::string contents;
+    if (!fs_utils::ReadContents(full_path, &contents)) {
+        return false;
+    }
+    for (const auto& line : absl::StrSplit(contents, '\n', absl::SkipWhitespace())) {
+        if (absl::StartsWith(line, "cpu ")) {
+            std::vector<std::string> items = absl::StrSplit(line, ' ');
+            // if (!absl::SimpleAtoi(absl::StripPrefix(line, "user "), user)) {
+            //     return false;
+            // }
+            DCHECK_EQ(items.size(), 12u);
+            if (!absl::SimpleAtoi(items[2], user)) {
+                return false;
+            }
+            if (!absl::SimpleAtoi(items[3], nice)) {
+                return false;
+            }
+            if (!absl::SimpleAtoi(items[4], system)) {
+                return false;
+            }
+            if (!absl::SimpleAtoi(items[5], idle)) {
+                return false;
+            }
+            if (!absl::SimpleAtoi(items[6], iowait)) {
+                return false;
+            }
+            if (!absl::SimpleAtoi(items[7], irq)) {
+                return false;
+            }
+            if (!absl::SimpleAtoi(items[8], softirq)) {
+                return false;
+            }
+            if (!absl::SimpleAtoi(items[9], steal)) {
+                return false;
+            }
+            if (!absl::SimpleAtoi(items[10], guest)) {
+                return false;
+            }
+            if (!absl::SimpleAtoi(items[11], guest_nice)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 }
 
 void SetCgroupFsRoot(std::string_view path) {
@@ -70,6 +128,22 @@ bool ReadContainerStat(std::string_view container_id, ContainerStat* stat) {
         return false;
     }
     if (!ReadCpuAcctStat(container_id, &stat->cpu_stat_user, &stat->cpu_stat_sys)) {
+        return false;
+    }
+    return true;
+}
+
+bool ReadProcStat(ProcStat* stat) {
+    if (!ReadCpuProcStat(&stat->cpu_user,
+                         &stat->cpu_nice,
+                         &stat->cpu_system,
+                         &stat->cpu_idle,
+                         &stat->cpu_iowait,
+                         &stat->cpu_irq,
+                         &stat->cpu_softirq,
+                         &stat->cpu_steal,
+                         &stat->cpu_guest,
+                         &stat->cpu_guest_nice)) {
         return false;
     }
     return true;
